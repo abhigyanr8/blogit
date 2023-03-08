823c5ef7eb556b525b5bae6458cd09cc0a3cd7fb
Abhigyan <abhigyanr8@gmail.com>
2023-03-08 18:43:56 +0530

    commit

---
+++ src/main/java/com/example/demo/Model/Post.java
@@ -0,0 +1,35 @@
+package com.example.demo.Model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(staticName="build")
+@Entity
+public class Post 
+{
+	@Id
+
+	private int id;
+	private String title;
+	private String content;
+	private String userId;
+	private String medialink;
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="pc_fid" ,referencedColumnName="id")
+	List<Comment> comments = new ArrayList<>();
+
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,62 @@
+package com.example.demo.service;
+
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.User;
+import com.example.demo.dto.UserRequest;
+import com.example.demo.repo.UserRepo;
+
+@Service
+public class UserService 
+{
+	
+	 @Autowired
+	 UserRepo userRepo;
+	 
+	 
+	 
+	//To add user
+	  
+	   public User addUser(UserRequest userRequest)
+	     {
+		     
+		       long id =  new Date().getTime();
+		       User user = User.build(id,userRequest.getName(),userRequest.getEmail(),userRequest.getPassword(),userRequest.getAbout(),null);
+		       System.out.println(id);
+		       System.out.println(new Date().getTime());
+		       return userRepo.save(user);
+	     }
+	   
+	//To List All Users
+	   
+	
+		public List<User>getAllUsers()
+		{
+			return userRepo.findAll();
+		}
+		
+	//To get User By Id
+		
+		
+		public Optional<User> getUserbyId(long id)
+		{
+			return userRepo.findById(id);
+		}
+		
+	//To delete User By Id
+				public void deleteUserById(long id)
+				{
+					userRepo.deleteById(id);
+				
+				}
+		
+	  
+
+}
